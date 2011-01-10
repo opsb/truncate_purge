b4985605fb7d6b73f0af41a6ea8a74288a979d76
opsb <oliver@opsb.co.uk>
2011-01-10 16:34:15 +0000

    Repackaged for use with rails2 as well as rails3

--- lib/truncate_purge.rb
+++ lib/truncate_purge.rb
@@ -1,9 +1,11 @@
 require 'rails'
 
-module TruncatePurge
-  class TaskLoader < Rails::Railtie
-    rake_tasks do
-      load File.dirname(__FILE__) + '/railties/tasks.rake'
+if Rails::VERSION::STRING =~ /^3/
+  module TruncatePurge
+    class TaskLoader < Rails::Railtie
+      rake_tasks do
+        load File.dirname(__FILE__) + '/tasks/truncate_purge.rake'
+      end
     end
   end
 end
