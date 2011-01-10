424de6ee67f14056e73beba0cbf38cb6dad118ab
opsb <oliver@opsb.co.uk>
2011-01-10 17:10:56 +0000

    For rails 2 - load rake tasks when gem is required

--- lib/truncate_purge.rb
+++ lib/truncate_purge.rb
@@ -1,11 +1,18 @@
 require 'rails'
 
-if Rails::VERSION::STRING =~ /^3/
+def load_tasks
+  load File.dirname(__FILE__) + '/tasks/truncate_purge.rake'
+end
+
+case Rails::VERSION::STRING
+when /^3/ 
   module TruncatePurge
     class TaskLoader < Rails::Railtie
       rake_tasks do
-        load File.dirname(__FILE__) + '/tasks/truncate_purge.rake'
+        load_tasks
       end
     end
   end
+when /^2/
+  load_tasks
 end
